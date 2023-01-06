0081caa09b441e838f8721a67fedcab57f86ef54
levtzuritamar <levtzuritamar@gmail.com>
2023-01-06 16:02:33 +0200

    m

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ with open('population_by_country.csv', 'r') as file:
     
     # Iterate through the rows of the CSV
     for row in reader:
-        country = row['Country_name']
-        population = row['Population']
+        print(row)
+        country = row['name']
+        population = row['population']
         
         # Insert the data into the MySQL table
         cursor.execute('INSERT INTO table (country, population) VALUES (%s, %s)', (country, population))
