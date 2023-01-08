7df751ffd6c11d7cb0c484683d47aa5acce0b516
levtzuritamar <levtzuritamar@gmail.com>
2023-01-08 16:08:02 +0200

    m

--- QUERIES-DB-SCRIPT.py
+++ QUERIES-DB-SCRIPT.py
@@ -13,5 +13,5 @@ cursor = cnx.cursor(buffered = True)
 # print(cursor.fetchmany(size=20))
 
 # cursor.execute("SELECT C.country FROM population as C, olympic_game_participants as OGP, athlete as A WHERE A.country = C.country AND OGP.athlete_id = A.athlete_id GROUP BY A.country ORDER BY SUM(A.athlete_id) DESC")
-cursor.execute("SELECT C.country FROM population as C, olympic_game_participants as OGP, athlete as A WHERE A.country = C.country AND OGP.athlete_id = A.athlete_id") 
+cursor.execute("SELECT C.country FROM population as C, olympic_game_participants as OGP, athlete as A WHERE A.country = C.country") 
 print(cursor.fetchmany(size=10))
