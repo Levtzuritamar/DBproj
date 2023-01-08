615dacdde3272fd22608172e11348dee93b52203
levtzuritamar <levtzuritamar@gmail.com>
2023-01-08 16:03:40 +0200

    m

--- QUERIES-DB-SCRIPT.py
+++ QUERIES-DB-SCRIPT.py
@@ -9,8 +9,8 @@ cnx = mysql.connector.connect(
 )
 
 cursor = cnx.cursor(buffered = True)
-cursor.execute("SELECT E.sport FROM olympic_game_participants as OGP, event AS E WHERE E.event = OGP.event Group by E.sport ORDER BY SUM(OGP.athlete_id) ASC")
-print(cursor.fetchmany(size=20))
+# cursor.execute("SELECT E.sport FROM olympic_game_participants as OGP, event AS E WHERE E.event = OGP.event Group by E.sport ORDER BY SUM(OGP.athlete_id) ASC")
+# print(cursor.fetchmany(size=20))
 
 cursor.execute("SELECT C.country FROM population as C, olympic_game_participants as OGP, athlete as A WHERE A.country = C.country AND OGP.athlete_id = A.athlete_id GROUP BY C.country ORDER BY SUM(A.athlete_id) DESC")
 print(cursor.fetchmany(size=10))
