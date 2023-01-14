1af81cf1719343a0db949459c9b0de3103816682
levtzuritamar <levtzuritamar@gmail.com>
2023-01-14 14:46:20 +0200

    m

--- QUERIES-DB-SCRIPT.py
+++ QUERIES-DB-SCRIPT.py
@@ -15,10 +15,10 @@ cursor = cnx.cursor(buffered = True)
 cursor.execute("SELECT athlete.country, FORMAT((COUNT(olympic_game_participants.athlete_id) / population.population)*100,3) AS participants_population_ratio FROM olympic_game_participants JOIN athlete ON olympic_game_participants.athlete_id = athlete.athlete_id JOIN population ON athlete.country = population.country GROUP BY athlete.country, population.population ORDER BY participants_population_ratio DESC LIMIT 10")
 print(cursor.fetchmany(size=10))
 
-# Second query: Sports by number of participants. We display the top 10.
+# Second query: Sports by number of participants
 cursor.execute("SELECT E.sport FROM olympic_game_participants as OGP, event AS E WHERE E.event = OGP.event Group by E.sport ORDER BY SUM(OGP.athlete_id) DESC LIMIT 10")
 print(cursor.fetchmany(size=10))
 
-# Third query: 
-cursor.execute("WITH medals_count AS (SELECT medals.country, SUM(medals.gold + medals.silver + medals.bronze) AS total_medals, population FROM medals JOIN population ON medals.country = population.country GROUP BY country, population) SELECT country, (total_medals / population) AS medals_population_ratio FROM medals_count ORDER BY medals_population_ratio DESC LIMIT 10")
+# Third query: Countries with the highest medal winnings and population ratio
+cursor.execute("WITH medals_count AS (SELECT medals.country, SUM(medals.gold + medals.silver + medals.bronze) AS total_medals, population FROM medals JOIN population ON medals.country = population.country GROUP BY country, population) SELECT country, FORMAT((total_medals / population)*100,3) AS medals_population_ratio FROM medals_count ORDER BY medals_population_ratio DESC LIMIT 10")
 print(cursor.fetchmany(size=10))
