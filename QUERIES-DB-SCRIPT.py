53b60788f97bd3cc12fae43b2f8c7ddb07e58855
levtzuritamar <levtzuritamar@gmail.com>
2023-01-14 15:04:06 +0200

    m

--- QUERIES-DB-SCRIPT.py
+++ QUERIES-DB-SCRIPT.py
@@ -11,15 +11,15 @@ cnx = mysql.connector.connect(
 cursor = cnx.cursor(buffered = True)
 
 # First query: Countries with highest amount of participants
-cursor.execute("SELECT athlete.country, COUNT(olympic_game_participants.athlete_id) AS total_participants FROM olympic_game_participants JOIN athlete ON olympic_game_participants.athlete_id = athlete.athlete_id  GROUP BY athlete.country ORDER BY total_participants DESC LIMIT 10")
+cursor.execute("SELECT athlete.country, COUNT(DISTINCT olympic_game_participants.athlete_id) AS total_participants FROM olympic_game_participants JOIN athlete ON olympic_game_participants.athlete_id = athlete.athlete_id  GROUP BY athlete.country ORDER BY total_participants DESC LIMIT 10")
 print(cursor.fetchmany(size=10))
 
 # Second query: Countries with the highest particiants and population ratio
-cursor.execute("SELECT athlete.country, FORMAT((COUNT(olympic_game_participants.athlete_id) / population.population)*100,3) AS participants_population_ratio FROM olympic_game_participants JOIN athlete ON olympic_game_participants.athlete_id = athlete.athlete_id JOIN population ON athlete.country = population.country GROUP BY athlete.country, population.population ORDER BY participants_population_ratio DESC LIMIT 10")
+cursor.execute("SELECT athlete.country, FORMAT((COUNT(DISTINCT olympic_game_participants.athlete_id) / population.population)*100,3) AS participants_population_ratio FROM olympic_game_participants JOIN athlete ON olympic_game_participants.athlete_id = athlete.athlete_id JOIN population ON athlete.country = population.country GROUP BY athlete.country, population.population ORDER BY participants_population_ratio DESC LIMIT 10")
 print(cursor.fetchmany(size=10))
 
 # Third query: Sports by number of participants
-cursor.execute("SELECT E.sport, SUM(OGP.athlete_id) as total_athletes FROM olympic_game_participants as OGP, event AS E WHERE E.event = OGP.event Group by E.sport ORDER BY total_athletes ASC LIMIT 10")
+cursor.execute("SELECT E.sport, SUM(DISTINCT OGP.athlete_id) as total_athletes FROM olympic_game_participants as OGP, event AS E WHERE E.event = OGP.event Group by E.sport ORDER BY total_athletes ASC LIMIT 10")
 print(cursor.fetchmany(size=10))
 
 # Fourth query: Countries with the highest medal winnings and population ratio
@@ -27,3 +27,5 @@ cursor.execute("WITH medals_count AS (SELECT medals.country, SUM(medals.gold + m
 print(cursor.fetchmany(size=10))
 
 # Fifth query: Countries with higest medal winnings and participants ratio
+cursor.execute("WITH medals_count AS (SELECT medals.country, SUM(medals.gold + medals.silver + medal.bronze) AS total_medals FROM medals GROUP BY country), total_participants_count AS (SELECT athlete.country, COUNT(DISTINCT athlete_id) as total_participants FROM olympic_game_participants JOIN athlete ON olympic_game_participants.athlete_id = athlete.athlete_id GROUP BY country) SELECT medals_count.country, (medals_count.total_medals / total_participants_count.total_participants) AS medals_participants_ratio FROM medals_count JOIN total_participants_count ON medals_count.country = total_participants_count.country ORDER BY medals_participants_ratio DESC LIMIT 10")
+print(cursor.fetchmany(size=10))
