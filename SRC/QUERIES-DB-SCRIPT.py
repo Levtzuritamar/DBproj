3c65b6599d32c137a7395b449c0508db4c5d7cf2
levtzuritamar <levtzuritamar@gmail.com>
2023-01-17 20:47:45 +0200

    m

--- SRC/QUERIES-DB-SCRIPT.py
+++ SRC/QUERIES-DB-SCRIPT.py
@@ -15,8 +15,8 @@ cursor = cnx.cursor(buffered = True)
 
 # Full text query:
 athlete_name = string.capwords(input("Favorite athlete: ")).strip()
-cursor.execute(f"SELECT * FROM athlete WHERE MATCH (name) AGAINST ('{athlete_name}' IN NATURAL LANGUAGE MODE)")
-df = pd.DataFrame(cursor.fetchmany(size=10), columns=["athlete_id", "name", "dob", "height", "weight", "sex", "country"])
+cursor.execute(f"SELECT * FROM athlete WHERE MATCH (name) AGAINST ('{athlete_name}') IN NATURAL LANGUAGE MODE)")
+df = pd.DataFrame(cursor.fetchmany(size=100), columns=["athlete_id", "name", "dob", "height", "weight", "sex", "country"])
 if df.empty:
   print(f"No data on athlete {athlete_name}")
 else:
